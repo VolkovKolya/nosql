e10d62105b613771f6ab3523caec3d17b3c8e159
FrostFlame <serazetdinow.damir@yandex.ru>
2018-10-13 11:07:16 +0300

    temp

---
+++ src/main/java/ru/kpfu/itis/group11501/cinema/entity/ActualStatistic.java
@@ -0,0 +1,35 @@
+package ru.kpfu.itis.group11501.cinema.entity;
+
+public class ActualStatistic {
+
+    private Long movieId;
+    private String countryName;
+    private String movieName;
+
+    public ActualStatistic() {
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+}
